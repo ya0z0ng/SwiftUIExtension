23970ed59cc4f8ed522769a62c791b13a366daad
Renaud Wang <l17y@Renauds-MacBook-Pro.local>
2025-02-09 00:04:32 +0800

    rename

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "swiftUIExtension",
+    name: "SwiftUIExtension",
     platforms: [
         .iOS(.v13),
         .tvOS(.v13),
@@ -13,8 +13,8 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "swiftUIExtension",
-            targets: ["swiftUIExtension"]),
+            name: "SwiftUIExtension",
+            targets: ["SwiftUIExtension"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -23,7 +23,7 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "swiftUIExtension",
+            name: "SwiftUIExtension",
             path: "Sources"),
 
     ]
